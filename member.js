0ac72f8533818195c6c6d7e3cc8a4855e4d6ac5c
ELI-student001 <student001@jinzaiedifist.sakura.ne.jp>
2025-05-12 06:02:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsmember() {
+    return (
+        <div className="skills">
+            <h1>Skills</h1>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>HTML & CSS</li>
+                <li>Python</li>
+            </ul>
+        </div>
+    );
+}
+
